df4a25316bbd7ba059c06a19a0e02e2152a39f99
Daniel Castellanos <danielecastellanos@gmail.com>
2021-11-18 16:56:11 -0500

    Codigos

--- TrabajoFinal/SIR.py
+++ TrabajoFinal/SIR.py
@@ -54,8 +54,8 @@ def plot_with_death_rate(S, I, R, t, divide_by=479853, death_rate=1.5):
 N = 479853 # poblaciçon de un país como España
  
 # Número inicial de infectados y recuperados, I0 and R0.
-I0 = 10000
-R0 = 0
+I0 = 0.625191
+R0 = 1.5
  
 # El resto, casi todo N, es susceptible de infectarse
 S0 = N - I0 - R0
@@ -65,7 +65,7 @@ beta = 0.06 # contagio
 gamma = 0.021 # recuperación
  
 # Pasos temporales (en días)
-t = np.linspace(0, 365, 365)
+t = np.linspace(0, 652, 652)
  
 # condiciones iniciales
 y0 = S0, I0, R0
@@ -76,4 +76,4 @@ S, I, R = ret.T
  
 plot(S, I, R, t) # Datos sin normalizar
 plot2(S, I, R, t, divide_by=N) # Datos normalizados
-#plot_with_death_rate(S, I, R, t, N, death_rate=1.5)
+plot_with_death_rate(S, I, R, t, N, death_rate=1.5)
